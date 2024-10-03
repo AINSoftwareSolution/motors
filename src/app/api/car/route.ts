ddb8536b2eac32fc6dc0d58eefcf862b1359d9ab
Naeema bargir <bargirn5@gmail.com>
2024-10-03 17:12:10 +0530

    dashboard added and api updated

--- src/app/api/car/route.ts
+++ src/app/api/car/route.ts
@@ -1,6 +1,6 @@
 // pages/api/car/route.ts
 import { connectMongoDB } from "@/app/lib/connect";
-import CarModel from "@/app/models/carModel";  
+import CarModel from "@/app/models/carModel";
 import { NextResponse } from "next/server";
 
 // GET: Fetch car listings
@@ -9,25 +9,34 @@ export async function GET() {
     await connectMongoDB();
     const cars = await CarModel.find();
     return NextResponse.json(cars, { status: 200 });
-  } catch (error:any) {
+  } catch (error: any) {
     return NextResponse.json(
-      { message: `An error occurred while fetching car data: ${error.message}` },
+      {
+        message: `An error occurred while fetching car data: ${error.message}`,
+      },
       { status: 500 }
     );
   }
 }
 
-// POST: Add a new car listing
 export async function POST(request: Request) {
   try {
+    // Parse car data from the request
     const carData = await request.json();
+    console.log("carData", carData);
+    // Connect to MongoDB
     await connectMongoDB();
+
+    // Save car data to the database
     await CarModel.create(carData);
+
+    // Return a success response
     return NextResponse.json(
       { message: "Car has been successfully listed.", status: 201 },
       { status: 201 }
     );
-  } catch (error:any) {
+  } catch (error: any) {
+    // Handle and return any errors that occur
     return NextResponse.json(
       { message: `An error occurred while adding the car: ${error.message}` },
       { status: 500 }
---
+++ src/app/portal/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "@/app/component/sidebar"
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <section>
+    <Sidebar />
+
+    <div className="p-4 sm:ml-64 dark:bg-gray-700">
+      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:bg-gray-800 dark:border-gray-700 mt-14">
+        {children}
+      </div>
+    </div>
+  </section>
+}
